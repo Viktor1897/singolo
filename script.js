8e1f9b285d43e68018d4da1d2a26296557e80099
Kolosov Viktor <vitykolosov@gmail.com>
2020-03-15 16:57:39 +0300

    feat: complete slider task part 2, dom

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+//HEADER
 let menuElements = document.querySelectorAll(".navigation_list > li > a");
-for (element of menuElements) {
+for (let element of menuElements) {
     element.onclick = function (e) {
         e.target.classList.add('colored');
         for (element of menuElements){
@@ -7,7 +8,7 @@ for (element of menuElements) {
         }
 }
 }
-
+//SLIDER
 let arrows = document.querySelectorAll(".arrow");
 let slide1 = document.querySelector(".slide_1");
 let slide2 = document.querySelector(".slide_2");
@@ -24,4 +25,31 @@ for (arrow of arrows) {
         }
         
     }
+}
+
+//SLIDER DISPLAYS in PHONES
+
+let buttonLeft = document.querySelector(".button1");
+let buttonRight = document.querySelector(".button2");
+let displayLeft = document.querySelector(".vertical_image");
+let displayRight = document.querySelector(".horisontal_image");
+let displayLeftOn = true;
+let displayRightOn = true;
+
+buttonLeft.onclick = function () {
+    if (displayLeftOn) {
+        displayLeft.style.opacity = 0;
+    } else {
+        displayLeft.style.opacity = 1 ;
+    }
+    displayLeftOn = !displayLeftOn;
+}
+
+buttonRight.onclick = function () {
+    if (displayRightOn) {
+        displayRight.style.opacity = 0;
+    } else {
+        displayRight.style.opacity = 1 ;
+    }
+    displayRightOn = !displayRightOn;
 }
